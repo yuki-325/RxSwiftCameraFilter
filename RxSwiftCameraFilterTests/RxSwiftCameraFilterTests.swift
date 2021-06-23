ab3adc21924866b5856c7d0881d95ac1cd775780
中野勇貴 <sakeikura325325@gmail.com>
2021-06-23 18:59:37 +0900

    Initial Commit

---
+++ RxSwiftCameraFilterTests/RxSwiftCameraFilterTests.swift
@@ -0,0 +1,33 @@
+//
+//  RxSwiftCameraFilterTests.swift
+//  RxSwiftCameraFilterTests
+//
+//  Created by 中野勇貴 on 2021/06/23.
+//
+
+import XCTest
+@testable import RxSwiftCameraFilter
+
+class RxSwiftCameraFilterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
