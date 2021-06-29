1866f47963c6e3081430aa27a2b9c81f0ac5e828
中野勇貴 <sakeikura325325@gmail.com>
2021-06-29 22:26:14 +0900

    [add] フォトライブラリに保存されている画像を表示する機能を追加

--- RxSwiftCameraFilter/PhotosCollectionViewController.swift
+++ RxSwiftCameraFilter/PhotosCollectionViewController.swift
@@ -11,18 +11,63 @@ import Photos
 
 class PhotosCollectionViewController: UICollectionViewController {
     
+    //フォトライブラリから取得した画像を入れる配列
+    private var images = [PHAsset]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         populatePhotos()
     }
     
     private func populatePhotos() {
         //フォトライブラリへのアクセス許可
-        PHPhotoLibrary.requestAuthorization { status in
+        PHPhotoLibrary.requestAuthorization { [weak self] status in
+            
             if status == .authorized {
                 //accese the photos form photo library
+                let assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
+                
+                assets.enumerateObjects { object, count, stop in
+                    self?.images.append(object)
+                }
+                
+                self?.images.reverse()
+                DispatchQueue.main.async {
+                    self?.collectionView.reloadData()
+                }
                 
             }
+            
         }
+    }
+}
+
+extension PhotosCollectionViewController {
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCollectionViewCell", for: indexPath) as? PhotoCollectionViewCell else {
+            fatalError("PhotoCollectionViewCell is not found.")
+        }
+        
+        let asset = self.images[indexPath.row]
+        let maneger = PHImageManager.default()
+        
+        maneger.requestImage(for: asset, targetSize: CGSize(width: 120, height: 120), contentMode: .aspectFill, options: nil) { image, _ in
+            
+            DispatchQueue.main.async {
+                cell.photoImageView.image = image
+            }
+        }
+        
+        return cell
     }
 }
