9cf7356b82a81939e06372f7be82d1ce5be1c75a
中野勇貴 <sakeikura325325@gmail.com>
2021-06-29 22:21:17 +0900

    [add] PhotoCollectionViewCell.swiftを追加

---
+++ RxSwiftCameraFilter/PhotoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCollectionViewCell.swift
+//  RxSwiftCameraFilter
+//
+//  Created by 中野勇貴 on 2021/06/29.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoImageView: UIImageView!
+}
