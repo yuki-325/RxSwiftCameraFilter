8003a8ca3620cb2b1fc5ab85e932722d4841ef63
中野勇貴 <sakeikura325325@gmail.com>
2021-06-29 20:36:35 +0900

    [add] PhotosCollectionViewController.swiftを追加

--- RxSwiftCameraFilter/PhotosCollectionViewController.swift
+++ RxSwiftCameraFilter/PhotosCollectionViewController.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import UIKit
+import Photos
+
+class PhotosCollectionViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        populatePhotos()
+    }
+    
+    private func populatePhotos() {
+        //フォトライブラリへのアクセス許可
+        PHPhotoLibrary.requestAuthorization { status in
+            if status == .authorized {
+                //accese the photos form photo library
+                
+            }
+        }
+    }
+}
