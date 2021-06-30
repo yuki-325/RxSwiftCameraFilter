08f871ed3d25f711e40e7513316d964c9ab2c8d1
中野勇貴 <sakeikura325325@gmail.com>
2021-06-30 20:52:46 +0900

    [add] PhotosCollectionViewで選択した画像を表示させる機能を追加: disposed用の変数 disposeBag, prepareメソッドを追加

--- RxSwiftCameraFilter/ViewController.swift
+++ RxSwiftCameraFilter/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard let navC = segue.destination as? UINavigationController,
+              let photosCVC = navC.viewControllers.first as? PhotosCollectionViewController
+        else {
+            fatalError("Segue destination is not found.")
+        }
+        
+        photosCVC.selectedPhoto.subscribe(onNext: { [weak self] photo in
+            
+            self?.photoImageView.image = photo
+            
+        }).disposed(by: disposeBag)
+    }
 
 }
 
